350ae5f06bd8b72867f0015e49aa8ed53090358e
Nate Eizenga <eizengan@gmail.com>
2022-08-02 22:02:51 -0400

    bundler new

---
+++ spec/lummox/core_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Lummox::Core do
+  it "has a version number" do
+    expect(Lummox::Core::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
