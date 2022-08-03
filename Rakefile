e31cddac22f100d10a8417a1662862657a73d949
Nate Eizenga <eizengan@gmail.com>
2022-08-03 19:48:32 -0400

    Better use of rspec

--- Rakefile
+++ Rakefile
@@ -4,9 +4,9 @@ require "bundler/gem_tasks"
 require "rspec/core/rake_task"
 require "rubocop/rake_task"
 
-RSpec::Core::RakeTask.new(:spec)
+RSpec::Core::RakeTask.new(:rspec)
 RuboCop::RakeTask.new(:rubocop) do |t|
   t.options = %w[--parallel --color]
 end
 
-task default: %i[spec rubocop]
+task default: %i[rubocop rspec]
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,57 @@
 # frozen_string_literal: true
 
+require "bundler/setup"
+require "pry-byebug"
 require "lummox/sdl"
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  # Show detailed output when only one file is run
+  config.default_formatter = :documentation if config.files_to_run.one?
 
   # Disable RSpec exposing methods globally on `Module` and `main`
+  # https://relishapp.com/rspec/rspec-core/docs/configuration/zero-monkey-patching-mode
   config.disable_monkey_patching!
 
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
+  # Failure tracking file for the `--only-failures` and `--next-failure` CLI options
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+
+    # Show all expectation messages when they are chained together
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    # Expecting/allowing method calls on nil is an error
+    mocks.allow_message_expectations_on_nil = false
+
+    # Disallow expecting/allowing unknown method calls
+    mocks.verify_partial_doubles = true
+  end
+
+  # Run specs in random order
+  config.order = :random
+
+  # Profile the 5 slowest examples
+  config.profile_examples = 5
+
+  # Metadata on shared contexts is added to contexts where it is included instead of triggering inclusion
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  # Show warnings
+  config.warnings = true
+
+  # Allows use of `--seed` to deterministically reproduce test failures related to randomization
+  Kernel.srand config.seed
+
+  # TRICKY: class instance variables persist between tests and should be cleared to avoid cross-test pollution,
+  # particularly in cases when they have been stubbed. We remove the ones present in the following array before
+  # every test just to be safe
+  class_instance_variables = []
+  config.prepend_before do
+    class_instance_variables.each do |klass, instance_variable|
+      klass.remove_instance_variable(instance_variable) if klass.instance_variable_defined?(instance_variable)
+    end
   end
 end
