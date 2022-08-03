6e7f163a89564c8b10a472c4e47effde70d3f256
Nate Eizenga <eizengan@gmail.com>
2022-08-02 22:50:50 -0400

    Add sdl2 basics

---
+++ lib/lummox/core/sdl.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "ffi"
+
+module Lummox
+  module Core
+    class SDL
+      LIB_SDL2 = "/usr/local/Cellar/sdl2/2.0.22/lib/libSDL2.dylib"
+
+      extend FFI::Library
+
+      ffi_lib_flags :now, :global
+      ffi_lib LIB_SDL2
+    end
+  end
+end
