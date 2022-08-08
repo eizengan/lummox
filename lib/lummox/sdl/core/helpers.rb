0644521557952f534d876645f93f88ff5520dfe0
Nate Eizenga <eizengan@gmail.com>
2022-08-07 21:25:20 -0400

    Incorporate pointers

--- lib/lummox/sdl/core/helpers.rb
+++ lib/lummox/sdl/core/helpers.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-module Lummox::SDL::Pointers
-  class Int < FFI::Struct
+module Lummox::SDL::Core::Helpers
+  class IntPtr < FFI::Struct
     layout  :value, :int
 
     def value
@@ -9,7 +9,7 @@ module Lummox::SDL::Pointers
     end
   end
 
-  class Float < FFI::Struct
+  class FloatPtr < FFI::Struct
     layout  :value, :float
 
     def value
