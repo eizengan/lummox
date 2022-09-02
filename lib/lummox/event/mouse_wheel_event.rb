1049450a6abb5c209af3673b8bd203f96daee681
Nate Eizenga <eizengan@gmail.com>
2022-09-02 16:44:47 -0400

    Revise MouseWheelEvent

--- lib/lummox/event/mouse_wheel_event.rb
+++ lib/lummox/event/mouse_wheel_event.rb
@@ -4,7 +4,7 @@ class Lummox::Event::MouseWheelEvent < Lummox::Event
   SDL_EVENT_FIELD = :mouse_wheel_event
 
   def_delegator :@sdl_event, :which, :mouse_id
-  def_delegators :@sdl_event, :window_id, :x, :y, :direction, :precise_x, :precise_y
+  def_delegators :@sdl_event, :window_id, :x, :direction, :precise_x
 
   def window
     Lummox::Window.from_id(window_id)
@@ -14,36 +14,15 @@ class Lummox::Event::MouseWheelEvent < Lummox::Event
     mouse_id == Lummox::SDL::Core::Mouse::TOUCH_MOUSE_ID
   end
 
-  def dx
-    directionize(x)
+  def y
+    -@sdl_event.y
   end
 
-  def dy
-    directionize(y)
-  end
-
-  def precise_dx
-    directionize(precise_x)
-  end
-
-  def precise_dy
-    directionize(precise_y)
+  def precise_y
+    -@sdl_event.precise_y
   end
 
   def inspect
-    "#<#{self.class} window_id=#{window_id} dx=#{dx} dy=#{dy}>"
-  end
-
-  private
-
-  def directionize(value)
-    case direction
-    when :mouse_wheel_normal
-      value
-    when :mouse_wheel_flipped
-      -value
-    else
-      raise Lummox::SDLError, "Unknown MouseWheelDirection #{direction}"
-    end
+    "#<#{self.class} window_id=#{window_id} x=#{x} y=#{y}>"
   end
 end
