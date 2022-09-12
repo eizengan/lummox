612d3ae97835c4099d8347d900c0b60ca5b890c7
Nate Eizenga <eizengan@gmail.com>
2022-09-11 20:27:29 -0400

    Extract Lummox::Window::DisplaySettings

--- lib/lummox/window.rb
+++ lib/lummox/window.rb
@@ -6,20 +6,19 @@ require "ffi"
 # IGNORE:
 # - set_window_modal_for - raises "That operation is not supported" MacOS; try Windows/Linux?
 
-# rubocop:disable Metrics/ClassLength
-
 class Lummox::Window
   WindowFlags = Lummox::Helpers::FlagSet.for(Lummox::SDL::WINDOW_FLAGS, prefix: :WINDOW_) # rubocop:disable Style/MutableConstant
 
   include Lummox::Helpers::InstanceRegistry
 
-  attr_reader :pointer, :title
+  attr_reader :pointer, :title, :display_settings
 
   def initialize(title, size:, position: nil, flags: [:opengl])
     position = translate_position(position)
 
     @title = title
     @pointer = create_managed_pointer(title, position, size, WindowFlags.new(*flags).value)
+    @display_settings = DisplaySettings.new(self)
     self.class.register_instance(pointer.address, self)
   end
 
@@ -42,57 +41,9 @@ class Lummox::Window
     Lummox::SDLError.raise_if(:zero?) { Lummox::SDL.get_window_id(pointer) }
   end
 
-  def fullscreen=(option)
-    if option
-      enable_fullscreen(desktop: option == :desktop)
-    else
-      disable_fullscreen
-    end
-    fullscreen?
-  end
-
-  def fullscreen?
-    window_flags_include?(:fullscreen)
-  end
-
-  def display_mode
-    display_mode = Lummox::DisplayMode.new
-    Lummox::SDLError.raise_if(:negative?) do
-      Lummox::SDL.get_window_display_mode(pointer, display_mode.sdl_display_mode)
-    end
-    display_mode
-  end
-
-  def display_mode=(display_mode)
-    Lummox::SDLError.raise_if(:negative?) { Lummox::SDL.set_window_display_mode(pointer, display_mode) }
-  end
-
-  def borderless=(borderless)
-    Lummox::SDL.set_window_bordered(pointer, :"#{!borderless}")
-    borderless?
-  end
-
-  def borderless?
-    window_flags_include?(:borderless)
-  end
-
-  def resizable=(resizable)
-    Lummox::SDL.set_window_resizable(pointer, :"#{resizable}")
-    resizable?
-  end
-
-  def resizable?
-    window_flags_include?(:resizable)
-  end
-
   def title=(new_title)
     Lummox::SDL.set_window_title(pointer, new_title)
     @title = new_title
-  end
-
-  def display
-    display_index = Lummox::SDLError.raise_if(:negative?) { Lummox::SDL.get_window_display_index(pointer) }
-    Lummox::Display.new(display_index)
   end
 
   def position
@@ -103,46 +54,6 @@ class Lummox::Window
 
   def position=(position)
     Lummox::SDL.set_window_position(pointer, *position)
-  end
-
-  def size
-    size_pointers = Array.new(2) { Lummox::SDL::IntPtr.new }
-    Lummox::SDL.get_window_size(pointer, *size_pointers)
-    size_pointers.map(&:value)
-  end
-
-  def size=(size)
-    Lummox::SDL.set_window_size(pointer, *size)
-  end
-
-  def maximum_size
-    size_pointers = Array.new(2) { Lummox::SDL::IntPtr.new }
-    Lummox::SDL.get_window_maximum_size(pointer, *size_pointers)
-    size_pointers.map { |p| p.null? ? nil : p.value }
-  end
-
-  def maximum_size=(size)
-    Lummox::SDL.set_window_maximum_size(pointer, *size)
-  end
-
-  def minimum_size
-    size_pointers = Array.new(2) { Lummox::SDL::IntPtr.new }
-    Lummox::SDL.get_window_maximum_size(pointer, *size_pointers)
-    size_pointers.map { |p| p.null? ? nil : p.value }
-  end
-
-  def minimum_size=(size)
-    Lummox::SDL.set_window_minimum_size(pointer, *size)
-  end
-
-  def opacity
-    opacity_pointer = Lummox::SDL::FloatPtr.new
-    Lummox::SDLError.raise_if(:negative?) { Lummox::SDL.get_window_opacity(pointer, opacity_pointer) }
-    opacity_pointer.value
-  end
-
-  def opacity=(opacity)
-    Lummox::SDLError.raise_if(:negative?) { Lummox::SDL.set_window_opacity(pointer, opacity) }
   end
 
   def minimize
@@ -190,35 +101,17 @@ class Lummox::Window
     Lummox::SDL.get_window_grab(pointer) == :true
   end
 
-  def border_sizes
-    size_pointers = {
-      top: Lummox::SDL::IntPtr.new,
-      left: Lummox::SDL::IntPtr.new,
-      bottom: Lummox::SDL::IntPtr.new,
-      right: Lummox::SDL::IntPtr.new
-    }
-    Lummox::SDLError.raise_if(:negative?) do
-      Lummox::SDL.get_window_borders_size(pointer, *size_pointers.values)
-    end
-    size_pointers.transform_values!(&:value)
+  def opacity=(opacity)
+    Lummox::SDLError.raise_if(:negative?) { Lummox::SDL.set_window_opacity(pointer, opacity) }
+  end
+
+  def opacity
+    opacity_pointer = Lummox::SDL::FloatPtr.new
+    Lummox::SDLError.raise_if(:negative?) { Lummox::SDL.get_window_opacity(pointer, opacity_pointer) }
+    opacity_pointer.value
   end
 
   private
-
-  def window_flags_include?(flag)
-    flags_value = Lummox::SDL.get_window_flags(pointer)
-    WindowFlags.new(value: flags_value).include?(flag)
-  end
-
-  def enable_fullscreen(desktop:)
-    flag_name = desktop ? :fullscreen_desktop : :fullscreen
-    flag_value = WindowFlags.new(flag_name).value
-    Lummox::SDLError.raise_if(:negative?) { Lummox::SDL.set_window_fullscreen(pointer, flag_value) }
-  end
-
-  def disable_fullscreen
-    Lummox::SDLError.raise_if(:negative?) { Lummox::SDL.set_window_fullscreen(pointer, 0) }
-  end
 
   def create_managed_pointer(title, position, size, flags)
     pointer = Lummox::SDLError.raise_if(:null?) { Lummox::SDL.create_window(title, *position, *size, flags) }
@@ -235,5 +128,3 @@ class Lummox::Window
     end
   end
 end
-
-# rubocop:enable Metrics/ClassLength
---
+++ lib/lummox/window/display_settings.rb
@@ -0,0 +1,119 @@
+# frozen_string_literal: true
+
+require "ffi"
+
+class Lummox::Window::DisplaySettings
+  attr_reader :window
+
+  def initialize(window)
+    @window = window
+  end
+
+  def display
+    display_index = Lummox::SDLError.raise_if(:negative?) { Lummox::SDL.get_window_display_index(window.pointer) }
+    Lummox::Display.new(display_index)
+  end
+
+  def fullscreen=(option)
+    if option
+      enable_fullscreen(desktop: option == :desktop)
+    else
+      disable_fullscreen
+    end
+    fullscreen?
+  end
+
+  def fullscreen?
+    window_flags_include?(:fullscreen)
+  end
+
+  def display_mode=(display_mode)
+    Lummox::SDLError.raise_if(:negative?) { Lummox::SDL.set_window_display_mode(window.pointer, display_mode) }
+  end
+
+  def display_mode
+    display_mode = Lummox::DisplayMode.new
+    Lummox::SDLError.raise_if(:negative?) do
+      Lummox::SDL.get_window_display_mode(window.pointer, display_mode.sdl_display_mode)
+    end
+    display_mode
+  end
+
+  def borderless=(borderless)
+    Lummox::SDL.set_window_bordered(window.pointer, :"#{!borderless}")
+    borderless?
+  end
+
+  def borderless?
+    window_flags_include?(:borderless)
+  end
+
+  def border_sizes
+    size_pointers = {
+      top: Lummox::SDL::IntPtr.new,
+      left: Lummox::SDL::IntPtr.new,
+      bottom: Lummox::SDL::IntPtr.new,
+      right: Lummox::SDL::IntPtr.new
+    }
+    Lummox::SDLError.raise_if(:negative?) do
+      Lummox::SDL.get_window_borders_size(pointer, *size_pointers.values)
+    end
+    size_pointers.transform_values!(&:value)
+  end
+
+  def resizable=(resizable)
+    Lummox::SDL.set_window_resizable(window.pointer, :"#{resizable}")
+    resizable?
+  end
+
+  def resizable?
+    window_flags_include?(:resizable)
+  end
+
+  def size=(size)
+    Lummox::SDL.set_window_size(window.pointer, *size)
+  end
+
+  def size
+    size_pointers = Array.new(2) { Lummox::SDL::IntPtr.new }
+    Lummox::SDL.get_window_size(window.pointer, *size_pointers)
+    size_pointers.map(&:value)
+  end
+
+  def maximum_size=(size)
+    Lummox::SDL.set_window_maximum_size(window.pointer, *size)
+  end
+
+  def maximum_size
+    size_pointers = Array.new(2) { Lummox::SDL::IntPtr.new }
+    Lummox::SDL.get_window_maximum_size(window.pointer, *size_pointers)
+    size_pointers.map { |p| p.null? ? nil : p.value }
+  end
+
+  def minimum_size=(size)
+    Lummox::SDL.set_window_minimum_size(window.pointer, *size)
+  end
+
+  def minimum_size
+    size_pointers = Array.new(2) { Lummox::SDL::IntPtr.new }
+    Lummox::SDL.get_window_maximum_size(window.pointer, *size_pointers)
+    size_pointers.map { |p| p.null? ? nil : p.value }
+  end
+
+  private
+
+  def window_flags_include?(flag)
+    flags_value = Lummox::SDL.get_window_flags(window.pointer)
+    Lummox::Window::WindowFlags.new(value: flags_value).include?(flag)
+  end
+
+  def enable_fullscreen(desktop:)
+    flag_name = desktop ? :fullscreen_desktop : :fullscreen
+    flag_value = WindowFlags.new(flag_name).value
+    Lummox::SDLError.raise_if(:negative?) { Lummox::SDL.set_window_fullscreen(window.pointer, flag_value) }
+  end
+
+  def disable_fullscreen
+    Lummox::SDLError.raise_if(:negative?) { Lummox::SDL.set_window_fullscreen(window.pointer, 0) }
+  end
+end
